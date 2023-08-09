ce77637da9739d80a566deb9bbb09f668a1ee80b
chaya <chayavogel1@gmail.com>
2023-08-09 09:28:30 -0400

    Just starting out

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+function createEmployeeRecord(employeeProfileArray) {
+    let obj = {
+        firstName: employeeProfileArray[0],
+        familyName: employeeProfileArray[1],
+        title: employeeProfileArray[2],
+        payPerHour: employeeProfileArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return obj
+}
+
+function createEmployeeRecords(employeesArray) {
+    for (const employee of employeesArray) {
+        const newObj = createEmployeeRecord(employee)
+        employeesArray.push(newObj)
+    }
+    return employeesArray
+}
