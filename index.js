fef6087e664c9e69b8720e9c1454c5ae40c42705
chaya <chayavogel1@gmail.com>
2023-08-09 14:08:09 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function createEmployeeRecord(employeeProfileArray) {
+function createEmployeeRecord(employeeRecordObj) {
     let obj = {
-        firstName: employeeProfileArray[0],
-        familyName: employeeProfileArray[1],
-        title: employeeProfileArray[2],
-        payPerHour: employeeProfileArray[3],
+        firstName: employeeRecordObj[0],
+        familyName: employeeRecordObj[1],
+        title: employeeRecordObj[2],
+        payPerHour: employeeRecordObj[3],
         timeInEvents: [],
         timeOutEvents: []
     }
@@ -11,9 +11,86 @@ function createEmployeeRecord(employeeProfileArray) {
 }
 
 function createEmployeeRecords(employeesArray) {
+    let newArray = []
     for (const employee of employeesArray) {
         const newObj = createEmployeeRecord(employee)
-        employeesArray.push(newObj)
+        newArray.push(newObj)
     }
-    return employeesArray
+    return newArray
+}
+
+function createTimeInEvent(employeeRecordObj, dateStamp) {
+    let hour = dateStamp.substring(11)
+    hour = parseInt(hour, 10)
+    const date = dateStamp.substring(0,10)
+    let obj = {
+        type: "TimeIn",
+        hour: hour,
+        date: date
+    }
+    const array = employeeRecordObj.timeInEvents
+    array.push(obj)
+    return employeeRecordObj
+}
+
+function createTimeOutEvent(employeeRecordObj, dateStamp) {
+    let hour = dateStamp.substring(11)
+    hour = parseInt(hour, 10)
+    const date = dateStamp.substring(0,10)
+    let obj = {
+        type: "TimeOut",
+        hour: hour,
+        date: date
+    }
+    const array = employeeRecordObj.timeOutEvents
+    array.push(obj)
+    return employeeRecordObj
+}
+
+function hoursWorkedOnDate(employeeRecordObj, date) {
+    const timeInArray = employeeRecordObj.timeInEvents
+    const correctObjTimeIn = timeInArray.find((obj) => obj.date === date)
+    const punchIn = correctObjTimeIn.hour
+
+    const timeOutArray = employeeRecordObj.timeOutEvents
+    const correctObjTimeOut = timeOutArray.find((obj) => obj.date === date)
+    const punchOut = correctObjTimeOut.hour
+
+    const hoursWorked = (punchOut - punchIn)/100
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(employeeRecordObj, date) {
+   const hours = hoursWorkedOnDate(employeeRecordObj, date)
+
+   const rate = employeeRecordObj.payPerHour
+   const wage = rate * hours
+
+   return wage
+}
+
+function allWagesFor(employeeRecordObj) {
+    const timeInArray = employeeRecordObj.timeInEvents
+    let array = []
+    for (const obj of timeInArray) {
+        let date = obj.date
+        const wage = wagesEarnedOnDate(employeeRecordObj, date)
+        array.push(wage)
+    }
+    const total = array.reduce(function(accumulator, currentWage) {
+       return accumulator + currentWage
+    })
+    return total
+}
+
+function calculatePayroll(arrayOfEmployeeRecordObjs) {
+    let array = []
+    for (const obj of arrayOfEmployeeRecordObjs) {
+        const employeeTotal = allWagesFor(obj)
+        array.push(employeeTotal)
+    }
+    const staffTotal = array.reduce(function(accumulator, currentTotal) {
+        return accumulator + currentTotal
+        })
+    return staffTotal
 }
